96a8a8e39e1341ac7d1d1af9fbc463ff0ada73eb
heartsucker <heartsucker@autistici.org>
2017-11-04 20:08:34 +0100

    2to3 on ./tests/test_journalist.py

--- securedrop/tests/test_journalist.py
+++ securedrop/tests/test_journalist.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from cStringIO import StringIO
+from io import StringIO
 import os
 import random
 import unittest
@@ -19,7 +19,7 @@ from db import (db_session, InvalidPasswordLength, Journalist, Reply, Source,
 import db
 import journalist
 import journalist_app.utils
-import utils
+from . import utils
 
 # Smugly seed the RNG for deterministic testing
 random.seed('¯\_(ツ)_/¯')
